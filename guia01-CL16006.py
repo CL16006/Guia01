3c850d0ba2e7d4d50d1db236fe0472703efe1eac
Fatima Calderon <cl16006@ues.edu.sv>
2020-09-09 19:02:11 -0600

    se le agregaron comentarios adicionales

--- guia01-CL16006.py
+++ guia01-CL16006.py
@@ -17,27 +17,31 @@ def piramide():
     altura = int(height)
     usados = 0
     j = 0
-
+    #se imprime la piramide
     while i < int(altura):
         print(" " * (altura - i), "* " * i)
         usados = usados + (1 * i)
         i += 1
-
+#se determina cuantos bloques han sobrado
     sobrantes = blocks - usados
     print('Bloques sobrantes', sobrantes)
 
 #piramide()
 def primo():
+    #se le pide un numero entero al usuario
     numero = int(input("Ingrese numero entero: "))
     x = 1
     contador = 0
+    # el numero 1 por definicion no es primo
     if (numero == 1):
         print('El numero',numero,'no es primo')
     else:
+#se determina cuantas veces se obtiene residuo cero al dividir entre todos los numeros menores al numero
         while x <= numero:
             if numero % x == 0:
                 contador += 1
             x += 1
+    #si se obtienen solo 2 veces residuo cero el numero es primo
         if contador == 2:
             print('El numero',numero,'es primo')
         else:
