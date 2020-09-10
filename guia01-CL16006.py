f21c55f905487dadd873b62935b60201faf1c953
Fatima Calderon <cl16006@ues.edu.sv>
2020-09-09 18:48:23 -0600

    el metodo piramide se creo perfectamente

--- guia01-CL16006.py
+++ guia01-CL16006.py
@@ -1,1 +1,29 @@
 #guia 01 de programacion3
+
+def piramide():
+    # usuario ingresa total de bloques y se crea piramide con esos bloques
+    blocks = int(input("ingrese numero de bloques: "))
+    # height es la altura de la piramide
+    height = 0
+    # suma es una variable que se ocupa para comparar
+    suma = 0
+
+    # ocupamos el while para sacar la altura que tendra nuestra piramide
+    while suma <= blocks:
+        height += 1
+        suma += height
+
+    i = 1
+    altura = int(height)
+    usados = 0
+    j = 0
+
+    while i < int(altura):
+        print(" " * (altura - i), "* " * i)
+        usados = usados + (1 * i)
+        i += 1
+
+    sobrantes = blocks - usados
+    print('Bloques sobrantes', sobrantes)
+
+#piramide()
